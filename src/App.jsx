9cfee00ff2090ef8728c4aac95bd7010f229ce0b
ahuang28 <amyhuang0605@gmail.com>
2023-11-05 17:44:49 -0500

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './views/header'
+import MainPage from './views/mainPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-red-400'>
-        Hello world
+      <div className='bg-[#F8F3ED] w-screen h-screen py-6 px-[73px]'>
+        <Header />
+        <MainPage />
       </div>
     </>
   )
---
+++ src/views/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+    <div className="w-full h-20 bg-white">
+        header
+    </div>
+    )
+}
+
+export default Header
