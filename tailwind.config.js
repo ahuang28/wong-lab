0a6319b29015be065d49ed7d138e1664e2ebc3bb
wen-wong <wen.wong@mail.mcgill.ca>
2023-11-06 18:04:21 -0500

    Fixed path issue on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx,jsx}", "components/**/*.{ts,tsx,jsx}"],
+  content: ["src/**/*.{ts,tsx,jsx}", "components/**/*.{ts,tsx,jsx}"],
   theme: {
     container: {
       center: true,
