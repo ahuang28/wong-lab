fcee8af2a69ab842ec1a79ac3aba956e85d1f305
ahuang28 <amyhuang0605@gmail.com>
2024-01-08 21:37:49 -0500

    finished iphone view

--- src/views/mobilePage.jsx
+++ src/views/mobilePage.jsx
@@ -13,6 +13,17 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from '../components/ui/carousel';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '../components/ui/accordion';
+import { Button } from '../components/ui/button';
+import { Mail } from 'lucide-react';
+import { Phone } from 'lucide-react';
+import { Linkedin } from 'lucide-react';
+
 
 
 function MobilePage() {
@@ -49,9 +60,9 @@ function MobilePage() {
                     </div>
                 </div>
                 <div className="relative top-[-18.25rem]" id="about"></div>
-                <div className="relative grid h-[36rem] bg-primary p-6">
+                <div className="relative grid h-[41rem] bg-primary px-6 pt-6">
                     <img
-                        className="absolute left-[-7rem] top-[-5rem] z-10 w-[27rem] h-[27rem]"
+                        className="absolute left-[-7rem] top-[5rem] z-10 w-[27rem] h-[27rem]"
                         src="/src/assets/microscope.svg"
                     />
                     <div>
@@ -67,19 +78,38 @@ function MobilePage() {
                         <img
                             src="/src/assets/tp_mugshot.png"
                             alt="logo"
-                            className="absolute bottom-[-5rem] right-[-1.5rem] h-[20rem] w-[20rem]"
+                            className="absolute bottom-0 right-[-1.5rem] h-[20rem] w-[20rem]"
                         />
                     </div>
                     
                 </div>
                 <div className="relative top-[-5rem]" id="projects"></div>
-                <div className="relative flex h-[40rem] w-full flex-col items-center bg-secondary pt-24">
+                <div className="relative flex w-full flex-col items-center bg-secondary pt-16">
                     <div className="flex flex-col items-center w-full px-14">
                         <div>
                             <div className="text-2xl font-bold">Projects</div>
                             <div className="mb-6 mt-2 h-2 w-12 bg-primary"></div>
                         </div>
-                        <Carousel className="w-full">
+                        {data.projects.map((project) => (
+                                        <Card className="min-h-[20rem] w-full min-w-[20rem] mb-6">
+                                            <CardHeader className="relative flex h-[10rem] flex-col justify-end rounded-t-lg p-3">
+                                                <img
+                                                    src={
+                                                        '/src/assets/' +
+                                                        project.background_img
+                                                    }
+                                                    className="absolute left-0 top-0 z-0 h-full w-full rounded-t-lg object-cover"
+                                                />
+                                                <CardTitle className="z-10 text-secondary">
+                                                    {project.title}
+                                                </CardTitle>
+                                            </CardHeader>
+                                            <CardContent className="text-ellipsis px-4 py-4">
+                                                {project.content}
+                                            </CardContent>
+                                        </Card>
+                            ))}
+                        {/* <Carousel className="w-full">
                         <CarouselContent>
                             {data.projects.map((project) => (
                                 <CarouselItem>
@@ -105,9 +135,153 @@ function MobilePage() {
                                 </CarouselItem>
                             ))}
                         </CarouselContent>
-                    </Carousel>
+                    </Carousel> */}
                     </div>
                 </div>
+                <div className="relative top-[-5rem]" id="team"></div>
+                <div className='w-full flex flex-col items-center bg-primary mt-10 px-8 pt-10'>
+                    <div>
+                        <div className='text-2xl font-bold text-secondary'>
+                                Meet the Team
+                        </div>
+                        <div className="mt-2 h-2 w-12 bg-accent"></div>
+                    </div>
+                    <div className="grid grid-cols-2 grid-rows-5 justify-items-center my-8">
+                        {data.team.map((member) => (
+                            <Card className="relative h-[16em] w-[11em] m-1 rounded-xl border-none">
+                                <img
+                                    src={'/src/assets/' + member.img}
+                                    alt="person face"
+                                    className="h-full w-full rounded-xl object-cover"
+                                />
+                                <div className="absolute bottom-0 h-1/2 w-full rounded-b-xl bg-gradient-to-t from-black to-transparent"></div>
+                                <div className="absolute bottom-6 left-4 text-secondary">
+                                    {member.name}
+                                </div>
+                                <div className="absolute bottom-2 left-4 text-sm text-secondary">
+                                    {member.role}
+                                </div>
+                            </Card>
+                        ))}
+                    </div>
+
+                </div>
+                <div className="relative top-[-5rem]" id="publications"></div>
+                <div className="flex flex-col items-center bg-secondary py-12 px-8">
+                    <div>
+                        <div className="text-2xl font-bold">Publications</div>
+                        <div className="mb-6 mt-2 h-2 w-12 bg-primary"></div>
+                    </div>
+                    <Accordion type="single" collapsible className="w-full">
+                        {
+                            data.publications.map((pub, index) => (
+                                <AccordionItem value={'item-' + index}>
+                                    <AccordionTrigger>
+                                        {pub.title}
+                                    </AccordionTrigger>
+                                    <AccordionContent>
+                                        <div className="text-gray-500">Published on {pub.date}</div>
+                                        <div className="font-bold">
+                                        {
+                                            pub.authors.map((author, index) => (
+                                                <span key={index}>
+                                                    {author}
+                                                    {index < pub.authors.length - 1 ? ', ' : ''}
+                                                </span>
+                                            ))
+                                        }
+                                        </div>
+                                        <div className="mt-2">{pub.content}</div>
+                                        <Button className="pl-0" variant="link" onClick={ () => window.open(pub.link) }>Learn more</Button>
+                                    </AccordionContent>
+                                </AccordionItem>
+                            ))
+                        }
+                    </Accordion>
+                </div>
+                <div className="relative top-[-5rem]" id="news"></div>
+                <div className="relative flex flex-col items-center bg-primary py-12">
+                    <div>
+                        <div className="text-2xl font-bold text-secondary">
+                            News
+                        </div>
+                        <div className="mb-10 mt-2 h-2 w-12 bg-accent"></div>
+                    </div>
+                    {
+                        data.news.carousel.length > 0 ? (
+                            <Carousel className="w-[43rem]">
+                                <CarouselContent>
+                                    { data.news.carousel.map((item, index) => (
+                                        <CarouselItem key={index}>
+                                            <div className="p-1">
+                                                <Card>
+                                                    <CardContent className="flex h-[24rem] items-center justify-center p-6">
+                                                        <span className="text-4xl font-semibold">
+                                                            {index + 1}
+                                                        </span>
+                                                    </CardContent>
+                                                </Card>
+                                            </div>
+                                        </CarouselItem>
+                                    ))}
+                                </CarouselContent>
+                                <CarouselPrevious />
+                                <CarouselNext />
+                            </Carousel>
+                        ) : (
+                            <div className="text-2xl font-bold text-secondary">
+                                { data.news.empty }
+                            </div>
+                        )
+                    }
+                </div>
+                <div
+                    className="relative p-8 border-y border-secondary bg-primary items-center"
+                    id="contact"
+                >
+                    <div className="flex flex-col items-center place-self-center">
+                        <div className="text-2xl font-bold text-secondary">
+                            Contact Me
+                        </div>
+                        <div className="mt-2 text-center text-secondary">
+                            If you are interested to collaborate, learn more or
+                            join our lab, send me a message using the
+                            information on the right.
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-center gap-4 pt-6">
+                            <a
+                                className="flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-secondary"
+                                href={ data.contact.mail.link }
+                            >
+                                <Mail className="h-5 w-5 stroke-primary" />
+                            </a>
+
+                            <a
+                                className="flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-secondary"
+                                href={ data.contact.phone.link }
+                            >
+                                <Phone className="h-5 w-5 stroke-primary" />
+                            </a>
+
+                            <a
+                                className="flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-secondary"
+                                href={ data.contact.linkedin.link }
+                            >
+                                <Linkedin className="h-5 w-5 stroke-primary" />
+                            </a>
+            
+                    </div>
+                </div>
+                <div className="relative flex items-center gap-20 bg-primary px-10 py-2">
+                    <div className="text-secondary">{ data.footer.copyright }</div>
+                    <div className="text-secondary">{ data.footer.mcgill }</div>
+                    <div className="text-secondary">{ data.footer.douglas }</div>
+                    <div className="text-secondary">
+                        { data.footer.design }
+                    </div>
+                </div>
+
 
             </div>
         
